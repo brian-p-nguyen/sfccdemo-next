9dd32f2bb1b807d69124dd337e377d584617a38b
brian-p-nguyen <92738318+brian-p-nguyen@users.noreply.github.com>
2022-04-20 11:22:08 -0400

    remove .next folder

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/products/[handle]":["static\u002Fchunks\u002Fpages\u002Fproducts\u002F[handle].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fproducts\u002F[handle]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
